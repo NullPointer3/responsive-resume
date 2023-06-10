c2147e930a7fb8812c8becd227137c35693b3f67
BitHero <nuhumusah874@gmail.com>
2023-06-10 15:18:38 +0000

    resume home

--- assets/script/main.js
+++ assets/script/main.js
@@ -21,3 +21,25 @@ function linkAction() {
     navMenu.classList.remove('show__menu')
 }
 navLink.forEach(link =>link.addEventListener('click', linkAction))
+
+/*=========== Scroll sections active link ====*/
+const sections = document.querySelectorAll('section[id')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        const sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }
+        else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+
+    })
+}
+window.addEventListener('scroll', scrollActive)
