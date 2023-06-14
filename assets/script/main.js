772d4295669d4491514fff01c8036da4f06ae7ac
BitHero <nuhumusah874@gmail.com>
2023-06-14 12:54:03 +0000

    Media Queries & scale-cv

--- assets/script/main.js
+++ assets/script/main.js
@@ -57,3 +57,33 @@ function scrollTop() {
     }
 }
 window.addEventListener('scroll', scrollTop)
+
+/*============= Dark/Light Theme =========*/
+const themeBtn = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+//previously selected topic 
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// we obtain the current theme that the interface has by validatin the dark theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeBtn.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+// We validate if the user previously chose a theme
+if(selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeBtn.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+
+}
+
+// activate or deactivate the theme manually with the button
+themeBtn.addEventListener('click', () => {
+    // add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeBtn.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
